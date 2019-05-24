d2ae395794c33055ac889b61a8625a500cf24561
sunspeedzy <84886255@qq.com>
2019-05-24 15:26:42 +0800

    基于 06_常量的本质含义与反编译及助记符详解 修改

---
+++ readme.txt
@@ -0,0 +1,7 @@
+这个工程的代码，是《深入理解JVM 张龙》的课程代码
+
+zy.jvm.classloader.MyTest1 用来演示Java类的主动使用和被动使用的区别
+zy.jvm.classloader.MyTest2 用来演示静态常量对Java类主动/被动使用的影响
+
+
+
---
+++ src/main/java/zy/jvm/classloader/MyTest2.java
@@ -0,0 +1,32 @@
+package zy.jvm.classloader;
+
+/**
+ * 常量在编译阶段会存入到调用这个常量的方法所在的类的常量池中
+ * 本质上，调用类并没有直接引用到定义常量的类，因此并不会触发定义常量的类的初始化
+ * 注意：这里指的是 MyParent2.str 这个常量存放到了MyTest2这个类的常量池中，之后MyTest2与MyParent2就没有任何关系了
+ *       甚至，可以在运行MyTest2之前，将MyParent2的class文件删除（这个试验要在命令行里做，否则会重新编译运行代码），
+ *       也不会影响运行结果
+ */
+
+public class MyTest2 {
+    public static void main(String[] args) {
+        System.out.println(MyParent2.str);
+        // 运行结果
+//        hello world
+        System.out.println(MyParent2.s);
+        System.out.println(MyParent2.i);
+        System.out.println(MyParent2.m);
+    }
+}
+
+class MyParent2 {
+    public static final String str = "hello world";
+    public static final short s = 7;
+    public static final int i = 128;
+    public static final int m = 1;
+
+    static {
+        System.out.println("MyParent2 static block");
+    }
+}
+
