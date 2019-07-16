88547f08ade52908536fc41c173512a2beb63396
sunspeedzy <84886255@qq.com>
2019-07-16 16:45:51 +0800

    基于 10_初始化对于类与接口的异同点深入解析 修改

--- src/main/java/zy/jvm/classloader/MyTest5.java
+++ src/main/java/zy/jvm/classloader/MyTest5.java
@@ -9,8 +9,8 @@ import java.util.Random;
  *
  * 当一个接口在初始化时，并不要求其父接口都完成了初始化。
  *
- * 在使用 接口的、在编译期内确定值的 静态常量时，不要求初始化这个接口及其父接口。
- * 在使用 接口的、在运行期内确定值的 静态常量时，要求初始化这个接口及其父接口。
+ * 在使用 接口的、在编译期内确定值的 静态常量时，就不会去加载这个接口，更不要求初始化这个接口及其父接口。
+ * 在使用 接口的、在运行期内确定值的 静态常量时，或者在使用实现类的非静态常量时，就会去加载这个接口及其父接口。
  * 对于接口来说，它只能定义静态常量。
  *
  * @author zhangyan_g
@@ -26,7 +26,7 @@ interface MyParent5 {
 //    public static final int a = new Random().nextInt(4);
 }
 
-interface MyChild5 extends MyParent5 {
-//    public static final int b = 6;
-    public static final int b = new Random().nextInt(4);
+class MyChild5 implements MyParent5 {
+    public static int b = 6;
+//    public static final int b = new Random().nextInt(4);
 }
