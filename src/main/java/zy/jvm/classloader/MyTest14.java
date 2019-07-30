246ea0190a8a4f1827d096a1781e6389347551f1
sunspeedzy <84886255@qq.com>
2019-07-31 06:38:47 +0800

    基于 13_不同的类加载器作用与加载动作剖析 修改

---
+++ src/main/java/zy/jvm/classloader/MyTest14.java
@@ -0,0 +1,45 @@
+package zy.jvm.classloader;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+
+public class MyTest14 {
+    public static void main(String[] args) throws IOException {
+        ClassLoader classLoader =
+                Thread.currentThread().getContextClassLoader();
+        // 获取当前线程的上下文类加载器，它会获取启动这个应用的类加载器
+
+//        System.out.println(classLoader);
+        // 运行结果：sun.misc.Launcher$AppClassLoader@18b4aac2
+
+        String resourceName = "zy/jvm/classloader/MyTest14.class";
+        // getResource() 根据资源名参数，找出所有的可以被字节码以某种方式访问到的代码资源，与代码位置无关
+        Enumeration<URL> urls = classLoader.getResources(resourceName);
+
+        while (urls.hasMoreElements()) {
+            URL url = urls.nextElement();
+            System.out.println(url);
+        }
+        /*
+        运行结果：
+file:/D:/gitRepo/jvm_lecture/out/production/classes/zy/jvm/classloader/MyTest14.class
+
+         */
+
+        System.out.println("--------------");
+
+        Class<?> clazz = MyTest14.class;
+        System.out.println(clazz.getClassLoader());
+        /*运行结果
+        sun.misc.Launcher$AppClassLoader@18b4aac2
+         */
+
+        clazz = String.class;
+        System.out.println(clazz.getClassLoader());
+        /*运行结果：   String属于由bootstrapClassLoader加载的类
+        null
+         */
+    }
+
+}
