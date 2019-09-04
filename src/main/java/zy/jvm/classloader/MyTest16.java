6aa55fde2e8c019a631d360c49742c0881e256d3
sunspeedzy <84886255@qq.com>
2019-09-04 23:39:29 +0800

    基于 15_自定义类加载器深入详解 修改

---
+++ src/main/java/zy/jvm/classloader/MyTest16.java
@@ -0,0 +1,114 @@
+package zy.jvm.classloader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * 自定义类加载器
+ * 1. 需要定义一个 ClassLoader 的名字
+ * 2. 可以给这个类加载器指定一个父加载器
+ * 3. 实现 findClass 方法，通过类名可以加载到这个类
+ */
+public class MyTest16 extends ClassLoader {
+
+    private String classLoaderName;
+
+    private final String fileExtension = ".class";
+
+    public MyTest16(String classLoaderName) {
+        // 使用getSystemClassLoader()方法返回的ClassLoader（即系统类加载器）作为
+        // parent ClassLoader来创建一个新的ClassLoader
+        super();
+        this.classLoaderName = classLoaderName;
+    }
+
+    /**
+     * 使用指定的parent类加载器作为委托的类加载器去创建一个新的类加载器。
+     * Creates a new class loader using the specified parent class loader for
+     * delegation.
+     *
+     *
+     * @param parent The parent class loader
+     * @throws SecurityException If a security manager exists and its
+     *                           <tt>checkCreateClassLoader</tt> method doesn't allow creation
+     *                           of a new class loader.
+     * @since 1.2
+     */
+    public MyTest16(ClassLoader parent, String classLoaderName) {
+        // 显示指定该类加载器的父加载器
+        super(parent);
+        this.classLoaderName = classLoaderName;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.classLoaderName + "]" ;
+    }
+
+    /**
+     * 根据类名寻找这个类，由 java.lang.ClassLoader 的 loadClass 方法调用
+     * @param className
+     * @return
+     * @throws ClassNotFoundException
+     */
+    @Override
+    protected Class<?> findClass(String className) throws ClassNotFoundException {
+        byte[] data = this.loadClassData(className);
+
+        return defineClass(className, data, 0, data.length);
+    }
+
+    /**
+     * 加载类数据，自定义的私有方法，由 findClass 方法调用
+     * 要读取类文件的字节数组
+     * @param name 要加载的类的名字
+     * @return
+     */
+    private byte[] loadClassData(String name) {
+        InputStream is = null;
+        // 存储返回值
+        byte[] data = null;
+        ByteArrayOutputStream baos = null;
+
+        try {
+            // 将类名中包的分隔符变为当前文件系统的分隔符
+            this. classLoaderName = this.classLoaderName.replace(".", File.separator);
+
+            is = new FileInputStream(new File(name + this.fileExtension));
+            baos = new ByteArrayOutputStream();
+
+            int ch = 0;
+            // 每次从输入流读取一个字节
+            while ( -1 != (ch = is.read())) {
+                baos.write(ch);
+            }
+            // 将字节数组输出流的内容直接转换为字节数组
+            data = baos.toByteArray();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                is.close();
+                baos.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        return data;
+    }
+
+    public static void test(ClassLoader classLoader) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Class<?> clazz = classLoader.loadClass("zy.jvm.classloader.MyTest1");
+        Object object = clazz.newInstance();
+
+        System.out.println(object);
+    }
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+        MyTest16 locader1 = new MyTest16("loader1");
+        test(locader1);
+    }
+}
